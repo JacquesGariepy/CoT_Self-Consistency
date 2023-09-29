7545f57951738f15232994fdb8a1acb81ecb0723
Jacques Gariépy <jacques.gariepy@outlook.com>
2023-09-28 23:14:20 -0400

    Update cot_self-consistency.py

--- cot_self-consistency.py
+++ cot_self-consistency.py
@@ -1,29 +1,25 @@
 import litellm
+from langchain.prompts import PromptTemplate
 
-# Initialize LiteLLM
-litellm = litellm.LiteLLM()
+class ChainOfThought:
+    def __init__(self, model="gpt-3.5-turbo", token_limit=150):
+        self.litellm = litellm.LiteLLM()
+        self.model = model
+        self.token_limit = token_limit
 
-def chain_of_thought_prompting(initial_prompt, iterations=5):
-    current_prompt = initial_prompt + ", think step-by-step"
-    messages = [
-        {"role": "system", "content": "you answer like a scientist in a brief and precise manner"},
-        {"role": "user", "content": current_prompt}
-    ]
-    
-    # Use LiteLLM for chat completion
-    response = litellm.completion(
-        model="gpt-3.5-turbo",  # specify model, adjust as needed
-        messages=messages,
-        max_tokens=150,
-        n=iterations,
-    )
-    
-    responses = [choice['message']['content'] for choice in response['choices'] if choice['message']['role'] == 'assistant']
-    responses_string = "first answer: " + ', next answer: '.join(responses)
-    
-    return responses_string
+    def prompt(self, initial_prompt, iterations=5):
+        template = PromptTemplate(f"{initial_prompt}, think step-by-step")
+        current_prompt = template.render()
+        response = self.litellm.completion(
+            model=self.model,
+            prompt=current_prompt,
+            max_tokens=self.token_limit,
+            n=iterations,
+        )
+        responses = [choice['message']['content'] for choice in response['choices'] if choice['message']['role'] == 'assistant']
+        return ", ".join(responses)
 
-# Usage example:
-initial_prompt = "Explain the process of photosynthesis"
-chain_of_thought = chain_of_thought_prompting(initial_prompt)
-print(chain_of_thought)
+# Usage
+thought_chain = ChainOfThought()
+print(thought_chain.prompt("Explain the process of photosynthesis"))
+
